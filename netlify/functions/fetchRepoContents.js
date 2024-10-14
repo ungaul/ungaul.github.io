b7d751ab9c0e71aa35c75bf569353b588f817c96
ungaul <debaux.julien@gmail.com>
2024-10-14 16:20:36 +0900

    Functions

--- netlify/functions/fetchRepoContents.js
+++ netlify/functions/fetchRepoContents.js
@@ -1,19 +1,34 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
     const { repo, path = '' } = event.queryStringParameters;
     const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-    const response = await fetch(`https://api.github.com/repos/ungaul/${repo}/contents/${path}`, {
-        headers: {
-            'Authorization': `token ${GITHUB_TOKEN}`
+    try {
+        const response = await fetch(`https://api.github.com/repos/ungaul/${repo}/contents/${encodeURIComponent(path)}`, {
+            headers: {
+                'Authorization': `token ${GITHUB_TOKEN}`
+            }
+        });
+
+        if (!response.ok) {  // Vérifie si la réponse est OK (status code 200)
+            return {
+                statusCode: response.status,
+                body: JSON.stringify({ error: 'Error fetching repo contents', message: response.statusText })
+            };
         }
-    });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-    };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error', message: error.message })
+        };
+    }
 };
--- netlify/functions/fetchUser.js
+++ netlify/functions/fetchUser.js
@@ -1,19 +1,34 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
     const username = 'ungaul';
     const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-    const response = await fetch(`https://api.github.com/users/${username}`, {
-        headers: {
-            'Authorization': `token ${GITHUB_TOKEN}`
+    try {
+        const response = await fetch(`https://api.github.com/users/${username}`, {
+            headers: {
+                'Authorization': `token ${GITHUB_TOKEN}`
+            }
+        });
+
+        if (!response.ok) {
+            return {
+                statusCode: response.status,
+                body: JSON.stringify({ error: 'Error fetching user', message: response.statusText })
+            };
         }
-    });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(data)
-    };
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error', message: error.message })
+        };
+    }
 };
